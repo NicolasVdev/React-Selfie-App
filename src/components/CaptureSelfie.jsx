87fd688519d74a9ae303111cfa1c8265f8a4b2b5
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-10-03 10:23:20 +0200

    test1

--- src/components/CaptureSelfie.jsx
+++ src/components/CaptureSelfie.jsx
@@ -1,13 +1,30 @@
-
 export const CaptureSelfie = async () => {
-  const canvas = document.createElement("canvas");
-  const video = document.querySelector("video");
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    const videoElement = document.createElement("video");
+    document.body.appendChild(videoElement);
+    videoElement.srcObject = stream;
 
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  const ctx = canvas.getContext("2d");
-  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    return new Promise((resolve) => {
+      videoElement.onloadedmetadata = async () => {
+        const canvas = document.createElement("canvas");
+        canvas.width = videoElement.videoWidth;
+        canvas.height = videoElement.videoHeight;
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+        const selfieDataUrl = canvas.toDataURL("image/jpeg");
 
-  const selfieDataUrl = canvas.toDataURL("image/jpeg");
-  // Stocker ou envoyer le selfie
+        // Arrêtez la vidéo et libérez le flux de la caméra
+        stream.getTracks().forEach((track) => track.stop());
+        videoElement.remove();
+
+        resolve(selfieDataUrl);
+      };
+
+      videoElement.play();
+    });
+  } catch (error) {
+    console.error("Erreur lors de l'accès à la caméra : ", error);
+    throw error;
+  }
 };
--- src/pages/Capture.jsx
+++ src/pages/Capture.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
-import { CaptureSelfie } from '../components/CaptureSelfie'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { CaptureSelfie } from '../components/CaptureSelfie';
 
 export const Capture = () => {
   const [selfie, setSelfie] = useState(null);
+
+  const handleCaptureSelfie = async () => {
+    const selfieDataUrl = await CaptureSelfie();
+    setSelfie(selfieDataUrl);
+  };
+
   return (
     <div>
-      <button onClick={CaptureSelfie}>Capturer Selfie</button>
+      <button onClick={handleCaptureSelfie}>Capturer Selfie</button>
       {selfie && <img src={selfie} alt="Selfie" />}
+      <Link to="/thanks">Terminer</Link>
     </div>
-  )
-}
+  );
+};
