a07a3555f708c3879196252fc40068b803585d5d
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-10-09 13:47:59 +0200

    save to database & adding supabase

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,70 @@ import * as yup from 'yup';
 import { useForm} from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useNavigate } from 'react-router-dom';
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_BASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
 
 export const Home = () => {
   const navigate = useNavigate();
-  const saveEmailToLocalStorage = (data) => {
+
+  const emailExists = async (email) => {
+    const { data, error } = await supabase
+      .from('emails')
+      .select()
+      .eq('email', email);
+    if (error) {
+      console.error("Erreur lors de la vérification de l'e-mail:", error);
+      return false;
+    }
+    return data.length > 0; // Si des données sont renvoyées, l'e-mail existe déjà
+  };
+
+  const addEmail = async (email) => {
+    if (await emailExists(email)) {
+      console.log("Cet e-mail existe déjà dans la base de données.");
+      return;
+    }
+    const { data, error } = await supabase.from('emails').insert([{ email }]);
+    if (error) {
+      console.error("Erreur lors de l'ajout de l'e-mail:", error);
+    } else {
+      console.log('E-mail ajouté avec succès:', email);
+    }
+  };
+
+  const saveEmailToDatabase = async (data) => {
     const { email } = data;
-    const existingEmails = JSON.parse(localStorage.getItem('emails')) || [];
-    if (existingEmails.includes(email)) {
+    const emailAlreadyExist = await emailExists(email);
+    if (emailAlreadyExist) {
       alert("Cet e-mail a déjà été utilisé, veuillez en choisir un autre");
       return;
     }
-    existingEmails.push(email);
-    localStorage.setItem('emails', JSON.stringify(existingEmails));
+    await addEmail(email);
     navigate("/capture");
   };
+
+  // const saveEmailToLocalStorage = (data) => {
+    // const { email } = data;
+  //   const existingEmails = JSON.parse(localStorage.getItem('emails')) || [];
+  //   if (existingEmails.includes(email)) {
+  //     alert("Cet e-mail a déjà été utilisé, veuillez en choisir un autre");
+  //     return;
+  //   }
+  //   existingEmails.push(email);
+  //   localStorage.setItem('emails', JSON.stringify(existingEmails));
+  //   addEmail(email);
+  //   navigate("/capture");
+  // };
+
   const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
   const schema = yup.object().shape({
     email: yup.string().required("L'email doit être requis").matches(emailRegex, "L'email doit être valide"),
   });
+
   const {register, handleSubmit, formState: {errors} } = useForm({
     resolver: yupResolver(schema)
   });
@@ -29,7 +75,7 @@ export const Home = () => {
       <h1>
         Home
       </h1>
-      <form className="" onSubmit={handleSubmit(saveEmailToLocalStorage)}>
+      <form className="" onSubmit={handleSubmit(saveEmailToDatabase)}>
         <input 
         type="text"
         placeholder='email@exemple.fr' 
