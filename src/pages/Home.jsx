b6e2ef93e545ae9a1a54dd2628631f961db8d56a
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-10-19 11:19:42 +0200

    starting front home page with tailwind & some css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,17 +30,32 @@ export const Home = () => {
 
   return (
     <>
-      <h1>
-        Home
+      <img src="https://les3fromages.com/wp-content/uploads/2022/04/LOGO_3F_2021web.png" className="h-100 w-300 flex justify-center" alt="Les 3 Fromages" title="LOGO_3F_2021web" data-src="https://les3fromages.com/wp-content/uploads/2022/04/LOGO_3F_2021web.png" />
+      <h1 className='text-white text-4xl font-bold flex justify-center py-10'>
+        Bienvenue au concert des 3 Fromages !
       </h1>
-      <form className="" onSubmit={handleSubmit(saveEmailToDatabase)}>
-        <input 
+      <div className='text-white text-justify font-semibold flex flex-col pb-12 text-2xl'>
+      <p className='flex justify-center pb-1'>
+        Pour l'occasion, nous vous proposons un concours de selfie.
+        </p>
+        <p className='flex justify-center pb-1'>
+        Afin de participer, rien de plus simple entre ton e-mail, prends-toi en photo et envoie le
+        </p>
+        <p className='flex justify-center'>
+        Un tirage au sort sera réalisé et tu recevras un e-mail pour t'informer du résultat.
+      </p>
+      </div>
+      <form className="flex flex-col justify-center" onSubmit={handleSubmit(saveEmailToDatabase)}>
+        <div className='flex justify-center text-center'>
+        <input
+        className='w-80 h-9 bg-transparent placeholder:text-whitesmoke text-white pl-1 input-email rounded-md' 
         type="text"
         placeholder='email@exemple.fr' 
         {...register('email')}
         />
-        {errors.email?.message && <p className="ml-1 pt-2 font-semibold text-red-500 text-sm">{errors.email?.message}</p>}
-        <input type="submit"  value="Suivant"/>
+        </div>
+        {errors.email?.message && <p className="flex justify-center py-2 font-medium text-red-500 text-lg">{errors.email?.message}</p>}
+        <input type="submit" className='text-white'  value="C'est à toi"/>
       </form>
     </>
   )
